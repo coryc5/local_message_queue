602dfefe62b6ca114e74d283dda9695d4d5e4551
coryc5 <14861205+coryc5@users.noreply.github.com>
2018-10-03 17:51:26 -0500

    add Cache and tests

---
+++ lib/local_message_queue/cache.ex
@@ -0,0 +1,56 @@
+defmodule LocalMessageQueue.Cache do
+  @moduledoc """
+  Map-based cache with an experation measured in milliseconds.
+  """
+
+  use Agent
+
+  @type cache_name :: Agent.name()
+
+  @doc false
+  @spec start_link({cache_name, pos_integer}) :: Agent.on_start()
+  def start_link({name, expiration_ms}) do
+    Agent.start_link(fn -> %{cache: %{}, expiration_ms: expiration_ms} end, name: name)
+  end
+
+  @doc """
+  Adds `value` to the cache under `key`.
+  """
+  @spec put(cache_name, any, any) :: :ok
+  def put(cache_name, key, value) do
+    Agent.update(cache_name, fn %{cache: cache, expiration_ms: expiration_ms} ->
+      now = get_now()
+      new_cache = Map.put(cache, key, {value, now + expiration_ms})
+
+      %{cache: new_cache, expiration_ms: expiration_ms}
+    end)
+  end
+
+  @doc """
+  Returns the cache's value under `key` if it exists and has not expired.
+  """
+  @spec get(cache_name, any) :: any
+  def get(cache_name, key) do
+    now = get_now()
+
+    Agent.get_and_update(cache_name, fn %{cache: cache} = state ->
+      case Map.get(cache, key) do
+        nil ->
+          {nil, state}
+
+        {_val, expiration} when expiration < now ->
+          updated_cache = Map.delete(cache, key)
+
+          {nil, %{state | cache: updated_cache}}
+
+        {value, _expiration} ->
+          {value, state}
+      end
+    end)
+  end
+
+  @spec get_now :: pos_integer
+  defp get_now do
+    DateTime.utc_now() |> DateTime.to_unix(:millisecond)
+  end
+end
---
+++ test/local_message_queue/cache_test.exs
@@ -0,0 +1,42 @@
+defmodule LocalMessageQueue.CacheTest do
+  use ExUnit.Case, async: true
+
+  alias LocalMessageQueue.Cache
+
+  @expiration_ms 200
+
+  setup do
+    registry = __MODULE__.Registry
+    start_supervised!({Registry, keys: :unique, name: registry})
+
+    name = {:via, Registry, {registry, System.unique_integer([:monotonic])}}
+
+    start_supervised!({Cache, {name, @expiration_ms}})
+
+    %{name: name}
+  end
+
+  test "returns nil if no value exists under key", %{name: name} do
+    assert Cache.get(name, :some_key) |> is_nil()
+  end
+
+  test "returns value if key exists", %{name: name} do
+    key = :some_key
+    value = :some_value
+
+    :ok = Cache.put(name, key, value)
+
+    assert Cache.get(name, key) == value
+  end
+
+  test "returns nil if item has expired", %{name: name} do
+    key = :some_key
+    value = :some_value
+
+    :ok = Cache.put(name, key, value)
+
+    :timer.sleep(@expiration_ms + 1)
+
+    assert Cache.get(name, key) |> is_nil()
+  end
+end
