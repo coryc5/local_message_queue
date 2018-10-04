36ac6b0564d75bbe2465a99df41329b096e20ec6
coryc5 <14861205+coryc5@users.noreply.github.com>
2018-10-04 13:16:34 -0500

    add Preloader and tests

---
+++ lib/local_message_queue/preloader.ex
@@ -0,0 +1,58 @@
+defmodule LocalMessageQueue.Preloader do
+  @moduledoc """
+  The `Preloader` subscribes to new messages and optionally checks to see if those messages have
+  already been processed and stored in a `Cache` by a `Consumer`. Those cached results are
+  dispatched as new messages and uncached messages are concatenated to the end of the Queue.
+  """
+
+  use GenServer
+
+  @type config :: %{
+          name: GenServer.name(),
+          subscription_key: atom,
+          publisher_key: atom,
+          registry: Registry.registry(),
+          cache: atom | nil,
+          queue: atom
+        }
+
+  @doc false
+  @spec start_link(config) :: GenServer.on_start()
+  def start_link(config) do
+    GenServer.start_link(__MODULE__, config, name: config.name)
+  end
+
+  @impl true
+  def init(config) do
+    LocalMessageQueue.listen(config.registry, config.subscription_key)
+
+    {:ok, config}
+  end
+
+  @impl true
+  def handle_info({:new_msgs, _publisher, msgs}, %{cache: cache} = config) do
+    {cached_results, uncached_msgs} =
+      case cache do
+        nil ->
+          {[], msgs}
+
+        cache ->
+          {cached_msgs, uncached_msgs} =
+            Enum.split_with(msgs, &LocalMessageQueue.Cache.get(cache, &1))
+
+          cached_results =
+            Enum.flat_map(cached_msgs, &LocalMessageQueue.Cache.get(config.cache, &1))
+
+          {cached_results, uncached_msgs}
+      end
+
+    LocalMessageQueue.dispatch_new_msgs(config.registry, config.publisher_key, cached_results)
+    LocalMessageQueue.Queue.concat(config.queue, uncached_msgs)
+
+    {:noreply, config}
+  end
+
+  def handle_info(_, config) do
+    {:noreply, config}
+  end
+end
---
+++ test/local_message_queue/preloader_test.exs
@@ -0,0 +1,97 @@
+defmodule LocalMessageQueue.PreloaderTest do
+  use ExUnit.Case, async: true
+
+  alias LocalMessageQueue.{Cache, TestHelpers}
+
+  @registry __MODULE__.Registry
+  @id __MODULE__
+  @subscription_key __MODULE__.NewNumber
+  @publisher_key __MODULE__.Doubled
+
+  setup context do
+    start_supervised!({Registry, keys: :unique, name: @registry})
+
+    cache_name =
+      case context[:cache] do
+        true ->
+          cache_name = {:via, Registry, {@registry, System.unique_integer([:monotonic])}}
+          expiration_ms = 200
+
+          start_supervised!({Cache, {cache_name, expiration_ms}})
+
+          cache_name
+
+        false ->
+          nil
+      end
+
+    queue_name = {:via, Registry, {@registry, System.unique_integer([:monotonic])}}
+
+    queue_config = %{
+      id: @id,
+      name: queue_name,
+      filter: nil,
+      registry: @registry
+    }
+
+    start_supervised!({LocalMessageQueue.Queue, queue_config})
+
+    name = {:via, Registry, {@registry, System.unique_integer([:monotonic])}}
+
+    config = %{
+      name: name,
+      registry: @registry,
+      subscription_key: @subscription_key,
+      publisher_key: @publisher_key,
+      cache: cache_name,
+      queue: queue_name
+    }
+
+    start_supervised!({LocalMessageQueue.Preloader, config})
+
+    %{queue_name: queue_name, cache_name: cache_name}
+  end
+
+  @tag cache: false
+  test "adds all messages to queue when cache not configured", %{queue_name: queue_name} do
+    # register as a listener to queue
+    :ok = LocalMessageQueue.listen_to_queue(@registry, @id)
+
+    # register as listener to publisher_key
+    :ok = LocalMessageQueue.listen(@registry, @publisher_key)
+
+    input = [1, 2, 3]
+
+    LocalMessageQueue.dispatch_new_msgs(@registry, @subscription_key, input)
+
+    assert_receive({:queue_add, ^queue_name})
+    assert LocalMessageQueue.Queue.len(queue_name) == length(input)
+
+    # preload dispatched no messages
+    refute_receive({:new_msgs, @publisher_key, _})
+  end
+
+  @tag cache: true
+  test "dispatches results of messages that have been cached and does not add them to the queue",
+       %{queue_name: queue_name, cache_name: cache_name} do
+    first_msg = 1
+    cached_result = [TestHelpers.double(first_msg)]
+    Cache.put(cache_name, first_msg, cached_result)
+
+    # register as a listener to queue
+    :ok = LocalMessageQueue.listen_to_queue(@registry, @id)
+
+    # register as listener to publisher_key
+    :ok = LocalMessageQueue.listen(@registry, @publisher_key)
+
+    input = [first_msg, 2, 3]
+
+    LocalMessageQueue.dispatch_new_msgs(@registry, @subscription_key, input)
+
+    assert_receive({:queue_add, ^queue_name})
+    assert LocalMessageQueue.Queue.len(queue_name) == length(input) - 1
+
+    # preloader dispatched cached results
+    assert_receive({:new_msgs, @publisher_key, cached_result})
+  end
+end
