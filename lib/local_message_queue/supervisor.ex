6719d73238b0489d70f85bef44b5b546b1991c97
coryc5 <14861205+coryc5@users.noreply.github.com>
2018-10-04 16:52:21 -0500

    add Supervisor and tests

---
+++ lib/local_message_queue/supervisor.ex
@@ -0,0 +1,90 @@
+defmodule LocalMessageQueue.Supervisor do
+  @moduledoc false
+
+  use Supervisor
+
+  alias LocalMessageQueue.{Cache, Consumer, Preloader, Queue}
+
+  @type config :: %{
+          name_registry: Registry.registry(),
+          message_registry: Registry.registry(),
+          id: atom,
+          cache_ttl: pos_integer | nil,
+          queue: map,
+          consumer: map,
+          subscription_key: atom,
+          publisher_key: atom
+        }
+
+  @spec start_link(config) :: Supervisor.on_start()
+  def start_link(config) do
+    Supervisor.start_link(__MODULE__, config, name: config.id)
+  end
+
+  @impl true
+  def init(config) do
+    children = [
+      {Queue, queue_config(config)},
+      {Consumer, consumer_config(config)},
+      {Preloader, preloader_config(config)}
+    ]
+
+    children_with_cache = cache_children(children, config)
+    Supervisor.init(children_with_cache, strategy: :one_for_all)
+  end
+
+  @spec child_spec(config) :: Supervisor.child_spec()
+  def child_spec(config) do
+    %{
+      id: config.id,
+      start: {__MODULE__, :start_link, [config]}
+    }
+  end
+
+  @spec cache_children([{module, term}], config) :: [{module, term}]
+  defp cache_children(other_children, %{cache_ttl: nil}), do: other_children
+
+  defp cache_children(other_children, %{cache_ttl: ttl} = config) do
+    [{Cache, {name(config, Cache), ttl * 1000}} | other_children]
+  end
+
+  @spec queue_config(config) :: Queue.config()
+  defp queue_config(%{queue: queue} = config) do
+    %{
+      id: config.id,
+      name: name(config, Queue),
+      registry: config.message_registry,
+      filter: Map.get(queue, :filter_fun)
+    }
+  end
+
+  @spec consumer_config(config) :: Consumer.config()
+  defp consumer_config(%{consumer: consumer} = config) do
+    %{
+      id: config.id,
+      name: name(config, Consumer),
+      registry: config.message_registry,
+      callback: consumer.callback,
+      publisher_key: config.publisher_key,
+      delay: Map.get(consumer, :delay),
+      cache: config.cache_ttl && name(config, Cache)
+    }
+  end
+
+  @spec preloader_config(config) :: Preloader.config()
+  defp preloader_config(config) do
+    %{
+      name: name(config, Preloader),
+      registry: config.message_registry,
+      subscription_key: config.subscription_key,
+      publisher_key: config.publisher_key,
+      queue: name(config, Queue),
+      cache: config.cache_ttl && name(config, Cache)
+    }
+  end
+
+  @spec name(config, atom) :: {:via, Registry, any}
+  defp name(config, process_type) do
+    {:via, Registry, {config.name_registry, {config.id, process_type}}}
+  end
+end
---
+++ test/local_message_queue/supervisor_test.exs
@@ -0,0 +1,51 @@
+defmodule LocalMessageQueue.SupervisorTest do
+  use ExUnit.Case, async: true
+
+  alias LocalMessageQueue.TestHelpers
+
+  @name_registry __MODULE__.NameRegistry
+  @msg_registry __MODULE__.MsgRegistry
+  @id __MODULE__
+  @subscription_key __MODULE__.NewNumber
+  @publisher_key __MODULE__.Doubled
+
+  setup do
+    start_supervised!({Registry, keys: :unique, name: @name_registry})
+    start_supervised!({Registry, keys: :duplicate, name: @msg_registry})
+
+    config = %{
+      name_registry: @name_registry,
+      message_registry: @msg_registry,
+      id: @id,
+      cache_ttl: nil,
+      queue: %{},
+      consumer: %{
+        delay: nil,
+        callback: {TestHelpers, :double}
+      },
+      subscription_key: @subscription_key,
+      publisher_key: @publisher_key
+    }
+
+    start_supervised!({LocalMessageQueue.Supervisor, config})
+
+    :ok
+  end
+
+  test "integration" do
+    # listen for published msgs
+    LocalMessageQueue.listen(@msg_registry, @publisher_key)
+
+    input = [1, 2, 3]
+
+    # dispatch a msg
+    LocalMessageQueue.dispatch_new_msgs(@msg_registry, @subscription_key, input)
+
+    # consumer should dispatch input doubled per the callback
+    Enum.each(input, fn msg ->
+      result = TestHelpers.double(msg)
+
+      assert_receive({:new_msgs, @publisher_key, [^result]})
+    end)
+  end
+end
