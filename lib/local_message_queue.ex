e8bc5b11caa014f78e2b095eb12e1a39c565e454
coryc5 <14861205+coryc5@users.noreply.github.com>
2018-10-21 21:42:45 -0500

    add doc with example config

--- lib/local_message_queue.ex
+++ lib/local_message_queue.ex
@@ -1,4 +1,48 @@
 defmodule LocalMessageQueue do
+  @moduledoc """
+  A local message queue for cacheable, asynchronous processing.
+
+  It allows developers to set up an asynchronous pipeline that is easily observable. Message queues
+  are groups of processes that use the `Registry` module to implement pub/sub behavior. Other
+  processes can subscribe to topics published by these queues to receive messages and check in on
+  queue sizes.
+
+  ## Processing flow
+
+  A LocalMessageQueue consists of several processes: `Preloader`, `Cache`, `Queue`, `Consumer`.
+  These four processes exist in their own supervision tree. The `Preloader` uses the `Registry`
+  to subscribe to messages. Upon receiving those messages, it will check if a `Cache` has been
+  enabled for this particular queue and if so, split out messages that exist unexpired in the cache
+  and immediately publish them. Messages that have not been cached are then added to the `Queue`.
+  The `Queue` will publish that it has had messages added to it, which will trigger the `Consumer`
+  to begin pulling messages from it. These messages are processed by a configured `Producer`
+  callback, and the results are added to the cache if it has been enabled. Finally, these results
+  are then published under the configured topic.
+
+  ## Example
+
+  The following map is an example of configuration that can be passed to a
+  `LocalMessageQueue.Supervisor`:
+
+      %{
+        name_registry: MyApp.LocalMessageQueue.NameRegistry,
+        message_registry: MyApp.LocalMessageQueue.MessageRegistry,
+        id: MyApp.Producers.Double,
+        cache_ttl: 5000,
+        queue: %{filter_fun: {MyApp.Producers.Double, :positive_number?}},
+        consumer: %{delay: nil},
+        producer: MyApp.Producers.Double,
+        subscription_key: MyApp.LocalMessageQueue.NewNumber,
+        publisher_key: MyApp.LocalMessageQueue.DoubledNumber
+      }
+
+  This queue would listen for messages either dispatched by other LocalMessageQueues or another
+  process using the `dispatch_new_msgs/3` fun with the right `:subscription_key`.
+  LocalMessageQueues subscribing to this queue's `:publisher_key` would automatically register as
+  listeners for those messages and other processes can register using the `listen/2` fun. This
+  particular queue includes a filter that would filter out non-positive numbers.
+  """
+
   @doc """
   Dispatches a message to all registered listeners.
   """
